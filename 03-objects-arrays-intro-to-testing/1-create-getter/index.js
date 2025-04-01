3f99a5b5c8ba02b9821fada547629e94ac72fb49
Gennady Belyaev M1 <gmbelyaev.work@gmail.com>
2025-04-01 18:31:08 +0300

    add solutions for 03-objects-arrays

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+    const keys = path.split('.');
+    return function (obj) {
+        let result = obj;
+        for (const key of keys) {
+          if (result == null || !Object.prototype.hasOwnProperty.call(result, key)) {
+            return undefined;
+          }
+          result = result[key];
+        }
+        return result;
+      };
 }
--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,12 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+    if (obj == null || typeof obj !== 'object') {
+        return undefined;
+    }
+    const inverted = {};
+    for (const [key, value] of Object.entries(obj)) {
+        inverted[value] = key;
+    }
+    return inverted;
 }
